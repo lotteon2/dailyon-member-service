d8072752169c705382490002bd89240817a4c78b
kkh <rlghks131@korea.ac.kr>
2023-11-28 15:57:27 +0900

    O2F-600 [Fix] Feign API 분리

--- src/main/java/com/dailyon/memeberservice/point/api/PointFeignApiController.java
+++ src/main/java/com/dailyon/memeberservice/point/api/PointFeignApiController.java
@@ -2,25 +2,28 @@ package com.dailyon.memeberservice.point.api;
 
 import com.dailyon.memeberservice.point.api.request.PointHistoryRequest;
 import com.dailyon.memeberservice.point.api.response.GetPointHistory;
-import com.dailyon.memeberservice.point.entity.PointHistory;
 import com.dailyon.memeberservice.point.service.PointService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/point")
+@RequestMapping("/clients/members/points")
 @CrossOrigin(origins = "*")
-public class PointApiController {
+public class PointFeignApiController {
 
     private final PointService pointService;
 
-    public PointApiController(PointService pointService) {
+    public PointFeignApiController(PointService pointService) {
         this.pointService = pointService;
     }
 
-    @GetMapping("/{id}")
-    public List<GetPointHistory> getPointHistory(@PathVariable Long id){
-        return pointService.getPointHistory(id);
+    @PostMapping("/use")
+    public void usePoint(@RequestBody PointHistoryRequest request){
+        pointService.usePoint(request);
+    }
+    @PostMapping("/add")
+    public void addPoint(@RequestBody PointHistoryRequest request){
+        pointService.addPoint(request);
     }
 }
