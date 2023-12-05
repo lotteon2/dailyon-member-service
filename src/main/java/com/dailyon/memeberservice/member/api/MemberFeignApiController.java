4a0875372bbc3f2b8cb2bbf9b2fbc4c32dc9f8fa
kkh <rlghks131@korea.ac.kr>
2023-12-05 18:53:58 +0900

    [Fix] 유레카 설정 수정

---
+++ src/main/java/com/dailyon/memeberservice/member/api/MemberFeignApiController.java
@@ -0,0 +1,25 @@
+package com.dailyon.memeberservice.member.api;
+
+import com.dailyon.memeberservice.member.api.request.MemberCreateRequest;
+import com.dailyon.memeberservice.member.api.request.MemberModifyRequest;
+import com.dailyon.memeberservice.member.api.response.MemberGetResponse;
+import com.dailyon.memeberservice.member.service.MemberService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/clients/members")
+@CrossOrigin(origins = "*")
+public class MemberFeignApiController {
+    private final MemberService memberService;
+
+    public MemberFeignApiController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Long> registerMember(@RequestBody MemberCreateRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(memberService.registerMember(request));
+    }
+}
