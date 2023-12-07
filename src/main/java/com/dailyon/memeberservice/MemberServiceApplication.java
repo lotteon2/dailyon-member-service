d9396e903f7cb22406c238a0d34d696ede1ff04e
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-07 17:01:35 +0900

    [FIX] add discovery client config

--- src/main/java/com/dailyon/memeberservice/MemberServiceApplication.java
+++ src/main/java/com/dailyon/memeberservice/MemberServiceApplication.java
@@ -2,10 +2,12 @@ package com.dailyon.memeberservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@EnableEurekaClient
 @EnableJpaAuditing
+@SpringBootApplication
 public class MemberServiceApplication {
 
     public static void main(String[] args) {
