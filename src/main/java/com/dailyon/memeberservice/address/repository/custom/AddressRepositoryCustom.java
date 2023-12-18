a5e382f388286e18ffb0c3cafb8cd52650c33dbf
AMolRang <rlghks131@korea.ac.kr>
2023-12-18 17:15:58 +0900

    O2F-279 [Fix] 조회 로직 변경

--- src/main/java/com/dailyon/memeberservice/address/repository/custom/AddressRepositoryCustom.java
+++ src/main/java/com/dailyon/memeberservice/address/repository/custom/AddressRepositoryCustom.java
@@ -1,9 +1,10 @@
 package com.dailyon.memeberservice.address.repository.custom;
 
 import com.dailyon.memeberservice.address.api.response.AddressGetResponse;
+import com.dailyon.memeberservice.address.entity.Address;
 
 import java.util.List;
 
 public interface AddressRepositoryCustom{
-    List<AddressGetResponse> findByMemberId(Long memberId);
+    List<Address> findByMemberId(Long memberId);
 }
