6b3d574c7acd54e9fa865a9b453422c437ad20fd
AMolRang <rlghks131@korea.ac.kr>
2023-12-28 14:52:30 +0900

    [Feat] 페이지네이션 추가

--- src/main/java/com/dailyon/memeberservice/point/api/PointApiController.java
+++ src/main/java/com/dailyon/memeberservice/point/api/PointApiController.java
@@ -1,7 +1,10 @@
 package com.dailyon.memeberservice.point.api;
 
+import com.dailyon.memeberservice.address.api.response.AddressGetResponse;
 import com.dailyon.memeberservice.point.api.response.GetPointHistory;
 import com.dailyon.memeberservice.point.service.PointService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +23,7 @@ public class PointApiController {
     }
 
     @GetMapping("")
-    public ResponseEntity<List<GetPointHistory>> getPointHistory(@RequestHeader(name = "memberId") Long memberId){
-       return ResponseEntity.status(HttpStatus.OK).body(pointService.getPointHistory(memberId));
+    public ResponseEntity<Page<GetPointHistory>> getPointHistory(@RequestHeader Long memberId, Pageable pageable){
+       return ResponseEntity.status(HttpStatus.OK).body(pointService.getPointHistory(memberId, pageable));
     }
 }
