68326ac0733c77dffb3cfeba213e27dd1b7bb533
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-30 17:04:46 +0900

    [FEAT] add timezone to seoul

--- src/main/java/com/dailyon/memeberservice/MemberServiceApplication.java
+++ src/main/java/com/dailyon/memeberservice/MemberServiceApplication.java
@@ -1,14 +1,13 @@
 package com.dailyon.memeberservice;
 
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import javax.annotation.PostConstruct;
-import java.util.Date;
-import java.util.TimeZone;
 
 @Slf4j
 @EnableEurekaClient
@@ -16,13 +15,13 @@ import java.util.TimeZone;
 @SpringBootApplication
 public class MemberServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MemberServiceApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(MemberServiceApplication.class, args);
+  }
 
-    @PostConstruct
-    public void setTimezoneToSeoul() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-        log.info("현재 시각={}", new Date());
-    }
+  @PostConstruct
+  public void setTimezoneToSeoul() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    log.info("현재 시각={}", LocalDateTime.now());
+  }
 }
