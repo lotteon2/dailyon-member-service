86e579651dab186816ad84e6b8a254ff79726e27
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-30 16:43:51 +0900

    [FEAT] add date logging

--- src/main/java/com/dailyon/memeberservice/MemberServiceApplication.java
+++ src/main/java/com/dailyon/memeberservice/MemberServiceApplication.java
@@ -1,13 +1,16 @@
 package com.dailyon.memeberservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
+import java.util.Date;
 import java.util.TimeZone;
 
+@Slf4j
 @EnableEurekaClient
 @EnableJpaAuditing
 @SpringBootApplication
@@ -20,5 +23,6 @@ public class MemberServiceApplication {
     @PostConstruct
     public void setTimezoneToSeoul() {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        log.info("현재 시각={}", new Date());
     }
 }
